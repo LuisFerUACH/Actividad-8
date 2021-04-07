10d63058c7f41a0efd61f1bfe06ca2862e74899c
Luis Fernando Félix Mata <51014165+LuisFerUACH@users.noreply.github.com>
2021-04-07 11:55:24 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const saludar = () => {
+  let  nombre = document.getElementById("nombre").value;
+
+
+(document.getElementById("masculino").checked) ? document.getElementById("respuesta").innerHTML =  "Bienvenido " + nombre : document.getElementById("respuesta").innerHTML =  "Bienvenida " + nombre ;
+
+
+
+
+
+
+}
